9cfbc28bc6b6db1807e003f97d4911d6d7a82649
yskatsumata <yskatsumata@gmail.com>
2016-05-06 00:15:58 +0900

    Navigate class hierarchies

---
+++ VisualBasic/01-Navigation/5-Navigate_class_hierarchies/5.3-Go_to_base_symbols.vb
@@ -0,0 +1,30 @@
+﻿Imports JetBrains.ReSharper.Koans.Navigation.ExampleCode
+
+Namespace JetBrains.ReSharper.Koans.Navigation
+    ' Go to Derived Symbols
+    '
+    ' Alt+Home (VS)
+    ' Ctrl+U (IntelliJ)
+
+    Public Class GoToBaseSymbols
+        Public Sub Method()
+            ' 1. Put the caret on Customer And Go To Base Symbols
+            '    Navigated to ICustomer
+            Dim customer As Customer = GetCustomer()
+
+            ' 2. Put the caret on SilverCustomer And Go To Base Symbols
+            '    Navigated to Customer.
+            '    Always navigates one level up the hierarchy, rather than
+            '    Go To Derived Symbols, which can navigate many levels down
+            Dim customer2 = New SilverCustomer("id", "Tim")
+
+            ' 3. Put the caret on PercentageDiscount And Go To Base Symbols
+            '    Navigated to virtual property Customer.PercentageDiscount
+            Console.WriteLine(customer2.PercentageDiscount)
+        End Sub
+
+        Private Function GetCustomer() As Customer
+            Return New Customer("id", "Matt")
+        End Function
+    End Class
+End Namespace
---
+++ VisualBasic/01-Navigation/5-Navigate_class_hierarchies/ExampleCode/ICustomer.vb
@@ -0,0 +1,49 @@
+﻿Namespace JetBrains.ReSharper.Koans.Navigation.ExampleCode
+    Public Interface ICustomer
+        ReadOnly Property Id As String
+        ReadOnly Property Name As String
+        ReadOnly Property PercentageDiscount As Integer
+    End Interface
+
+    Public Class Customer
+        Implements ICustomer
+
+        Public Sub New(id As String, name As String)
+            Me.Id = id
+            Me.Name = name
+            Me.PercentageDiscount = 0
+        End Sub
+
+        Public ReadOnly Property Id As String Implements ICustomer.Id
+        Public ReadOnly Property Name As String Implements ICustomer.Name
+        Public Overridable ReadOnly Property PercentageDiscount As Integer Implements ICustomer.PercentageDiscount
+    End Class
+
+    Public Class SilverCustomer
+        Inherits Customer
+
+        Public Sub New(id As String, name As String)
+            MyBase.New(id, name)
+        End Sub
+
+        Public Overrides ReadOnly Property PercentageDiscount As Integer
+            Get
+                Return 10
+            End Get
+        End Property
+    End Class
+
+    Public Class GoldCustomer
+        Inherits Customer
+
+        Public Sub New(id As String, name As String)
+            MyBase.New(id, name)
+        End Sub
+
+        Public Overrides ReadOnly Property PercentageDiscount As Integer
+            Get
+                Return 25
+            End Get
+        End Property
+    End Class
+End Namespace
